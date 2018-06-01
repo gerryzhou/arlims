d15c2986e97fa59033c5e1336c01ec08d99331ff
Steve Harris <stephen.harris@fda.hhs.gov>
2018-06-01 15:46:56 -0500

    Notes on FACTS schema added, minor tweaks of ARL lims schema.

---
+++ src/main/java/gov/fda/nctr/arlims/models/dto/Analyst.java
@@ -0,0 +1,13 @@
+package gov.fda.nctr.arlims.models.dto;
+
+public class Analyst
+{
+    private long id;
+    private String factsId;
+    private String shortName;
+    private String facilityName;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String middleNameOrInitial;
+}
