c41b0dee3ff3c389c5b1c25326036e8f1142915a
Steve Harris <stephen.harris@fda.hhs.gov>
2018-06-27 14:15:59 -0500

    Tweak init code.

--- client/src/app/shared/services/user.service.ts
+++ client/src/app/shared/services/user.service.ts
@@ -2,22 +2,30 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
 import {ApiUrlsService} from './api-urls.service';
-import {UserContext} from "../../../generated/dto";
+import {UserContext, LabGroupContents, AuthenticatedUser} from '../../../generated/dto';
 
 @Injectable()
 export class UserService {
 
-   public userContext: UserContext;
+   public authenticatedUser: AuthenticatedUser;
+   public labGroupContents: LabGroupContents;
 
-   constructor(private apiUrlsSvc: ApiUrlsService, private httpClient: HttpClient) { }
+   constructor(private apiUrlsSvc: ApiUrlsService, private httpClient: HttpClient) {}
 
+   initWithUserContext(userContext: UserContext) {
+      this.authenticatedUser = userContext.authenticatedUser;
+      this.labGroupContents = userContext.labGroupContents;
+      // TODO: Initialize lookup maps etc from the LabGroupContents as needed (e.g. short name -> UserReference) .
+   }
+
+   // Called via app-module to initialize the service prior to usage.
    loadUserContext(): Promise<UserContext> {
-      let ucProm: Promise<UserContext> =
+      const ucProm: Promise<UserContext> =
          this.httpClient
          .get<UserContext>(this.apiUrlsSvc.userContextUrl())
          .toPromise();
 
-      ucProm.then(userCtx => this.userContext = userCtx);
+      ucProm.then(userCtx => this.initWithUserContext(userCtx));
 
       return ucProm;
    }
