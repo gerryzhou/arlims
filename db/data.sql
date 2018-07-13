f4252af3144964368d8232e9db447c423090e6f6
Steve Harris <stephen.harris@fda.hhs.gov>
2018-07-13 11:11:47 -0500

    Remove resource lists from one of the example samples.

--- db/data.sql
+++ db/data.sql
@@ -109,22 +109,9 @@ insert into sample_assignment(sample_id, employee_id, assigned_date, lead)
   values(5, 1, '24 Jun 2018', 1);
 
 insert into sample_managed_resource (sample_id, employee_id, list_name, resource_code)
-  values(1, 1, 'golanga SLM - Jul 3, 2018', 'ARL00424');
-insert into sample_managed_resource (sample_id, employee_id, list_name, resource_code)
-  values(1, 1, 'golanga SLM - Jul 3, 2018', 'ARL00190');
-insert into sample_managed_resource (sample_id, employee_id, list_name, resource_code)
   values(2, 1, 'shrimp - Jul 5, 2018', 'ARL00424');
 insert into sample_managed_resource (sample_id, employee_id, list_name, resource_code)
   values(2, 1, 'shrimp - Jul 5, 2018', 'ARL00190');
-
-insert into sample_unmanaged_resource (sample_id, employee_id, list_name, resource_code, resource_type)
-  values(1, 1, 'golanga media - Jul 3, 2018', 'RT-123456', 'MED');
-insert into sample_unmanaged_resource (sample_id, employee_id, list_name, resource_code, resource_type)
-  values(1, 1, 'golanga media - Jul 3, 2018', 'TT-234567', 'MED');
-insert into sample_unmanaged_resource (sample_id, employee_id, list_name, resource_code, resource_type)
-  values(1, 1, 'golanga media (additional)', 'RT-654321', 'MED');
-insert into sample_unmanaged_resource (sample_id, employee_id, list_name, resource_code, resource_type)
-  values(1, 1, 'golanga media (additional)', 'TT-234234', 'MED');
 
 insert into sample_unmanaged_resource (sample_id, employee_id, list_name, resource_code, resource_type)
   values(2, 1, 'shrimp media - Jul 3, 2018', 'RT-234567', 'MED');
