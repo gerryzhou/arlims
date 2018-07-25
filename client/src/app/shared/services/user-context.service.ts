06fa16a7aaaad4b0f1e0ddbb20d7e7e0edf97a24
Steve Harris <stephen.harris@fda.hhs.gov>
2018-07-25 07:57:46 -0500

    Sketch new user context service design in notes.

--- client/src/app/shared/services/user-context.service.ts
+++ client/src/app/shared/services/user-context.service.ts
@@ -12,38 +12,70 @@ export class UserContextService {
 
    public authenticatedUser: AuthenticatedUser;
 
-   public labGroupContents: LabGroupContents;
-   private labGroupContentsReloadRequested = false;
+   // TODO: Expose as ReplaySubject(1).
+   private labGroupContents$: Observable<LabGroupContents>;
 
    private labGroupContentsLastUpdated: Date;
-   private testIdToSampleInTest: Map<number, SampleInTest>;
 
+   // Make these readonly ReplaySubject(1)s.
+   private testIdToSampleInTest: Map<number, SampleInTest>;
    private testTypeCodeToLabGroupTestConfigJson: Map<string, string>;
 
    constructor(private apiUrlsSvc: ApiUrlsService, private httpClient: HttpClient) {}
 
    public getLabGroupContents(): Observable<LabGroupContents> {
-      return obsof(this.labGroupContents);
-   }
-
-   requestLabGroupContentsReload() {
-      this.labGroupContentsReloadRequested = true;
-   }
-
-   getSampleInTest(testId: number): SampleInTest | undefined {
-      return this.testIdToSampleInTest.get(testId);
-   }
-
-   getLabGroupTestConfigJson(testTypeCode: string): string | undefined {
-      return this.testTypeCodeToLabGroupTestConfigJson.get(testTypeCode);
+      return this.labGroupContents$;
    }
 
    private setLabGroupContents(lgContents: LabGroupContents) {
-      this.labGroupContents = lgContents;
+      this.labGroupContents$ = obsof(lgContents);
       this.labGroupContentsLastUpdated = new Date();
+
+      // TODO: Remove or make observable.
       this.testIdToSampleInTest = this.getSampleInTestsByTestId(lgContents.activeSamples);
       this.testTypeCodeToLabGroupTestConfigJson = this.getLabGroupTestConfigJsonsByTestTypeCode(lgContents.supportedTestTypes);
    }
+
+   requestLabGroupContentsReload() {
+      this.labGroupContents$ =
+         this.httpClient.get<UserContext>(this.apiUrlsSvc.userContextUrl()).pipe(
+            map(userContext => userContext.labGroupContents)
+         );
+      this.labGroupContents$.subscribe(newLabGroupContents => {
+         this.setLabGroupContents(newLabGroupContents);
+      });
+   }
+
+   ///////////////////////////////////////////////
+   // TODO: Expose as observable based on access of the Map ReplaySubject member.
+   getSampleInTest(testId: number): SampleInTest | undefined {
+      return this.testIdToSampleInTest.get(testId);
+   }
+   // TODO: Expose as observable based on access of the Map ReplaySubject member.
+   getLabGroupTestConfigJson(testTypeCode: string): string | undefined {
+      return this.testTypeCodeToLabGroupTestConfigJson.get(testTypeCode);
+   }
+   // TODO: Make available as ReplaySubject(1).
+   private getSampleInTestsByTestId(samples: Sample[]) {
+      const m = new Map<number, SampleInTest>();
+      for (const s of samples) {
+         for (const t of s.tests) {
+            m.set(t.testId, { sample: s, testMetadata: t });
+         }
+      }
+      return m;
+   }
+   // TODO: Make available as ReplaySubject(1).
+   private getLabGroupTestConfigJsonsByTestTypeCode(testTypes: LabTestType[]): Map<string, string> {
+      const m = new Map<string, string>();
+      for (const testType of testTypes) {
+         if (testType.configurationJson) {
+            m.set(testType.code, testType.configurationJson);
+         }
+      }
+      return m;
+   }
+   ///////////////////////////////////////////////
 
    // Called via app-module to initialize the service prior to usage.
    loadUserContext(): Promise<UserContext> {
@@ -62,23 +94,4 @@ export class UserContextService {
       this.setLabGroupContents(userContext.labGroupContents);
    }
 
-   private getSampleInTestsByTestId(samples: Sample[]) {
-      const m = new Map<number, SampleInTest>();
-      for (const s of samples) {
-         for (const t of s.tests) {
-            m.set(t.testId, { sample: s, testMetadata: t });
-         }
-      }
-      return m;
-   }
-
-   private getLabGroupTestConfigJsonsByTestTypeCode(testTypes: LabTestType[]): Map<string, string> {
-      const m = new Map<string, string>();
-      for (const testType of testTypes) {
-        if (testType.configurationJson) {
-           m.set(testType.code, testType.configurationJson);
-        }
-      }
-      return m;
-   }
 }
