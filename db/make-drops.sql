6da648556c754399da7684c64e3b86e11fa2e3de
Steve Harris <stephen.harris@fda.hhs.gov>
2019-01-31 10:47:49 -0600

    Omit making drop commands for context index tables.

--- db/make-drops.sql
+++ db/make-drops.sql
@@ -1,6 +1,7 @@
 select 'drop table ' || table_name || ' cascade constraints purge;' drop_command
 from user_tables
-union all 
+where table_name not like 'DR$%'
+union all
 select 'drop view ' || view_name || ';' drop_command
 from user_views
 ;
