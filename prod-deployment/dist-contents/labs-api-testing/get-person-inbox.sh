9321f8390f2770575a5c546fed9aebe6bae573d6
Steve Harris <stephen.harris@fda.hhs.gov>
2019-02-21 10:38:41 -0600

    Allow specifying person id for person inbox test script.

--- prod-deployment/dist-contents/labs-api-testing/get-person-inbox.sh
+++ prod-deployment/dist-contents/labs-api-testing/get-person-inbox.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 rand=$(((RANDOM % 10000)))
+personid=${1:-472629}
 curl \
   -H "Accept: application/json" \
   -H "Authorization: Basic <SEE APP DEBUG LOG FOR VALUE>" \
   -H "sourceApplicationId: 0757" \
   -H "sourceTransactionId: A3WDBHDA${rand}" \
-  "http://dih-svc.test.fda.gov/LabsDataService/api/PersonInbox?personId=472629&statusCodes=S,T,I&maxRecords=10"
+  "http://dih-svc.test/LabsDataService/api/PersonInbox?personId=$personid&statusCodes=S,T,I&maxRecords=50&allowNullValues=Y"
