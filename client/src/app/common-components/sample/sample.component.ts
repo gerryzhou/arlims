f4771bab31a8f97f89f5a72da818af8eaec4870b
Steve Harris <stephen.harris@fda.hhs.gov>
2019-03-05 09:05:23 -0600

    Make datepickers more touch friendly and tweak layout and default in new test dialog.

--- client/src/app/common-components/sample/sample.component.ts
+++ client/src/app/common-components/sample/sample.component.ts
@@ -94,7 +94,7 @@ export class SampleComponent implements OnChanges {
             sampleOp: this.sampleOp,
             availableTestTypes: this.labGroupTestTypes,
             selectedTestType: null,
-            beginDate: null,
+            beginDate: moment(),
          }
       });
 
